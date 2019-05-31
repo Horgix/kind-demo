4dadc3b4f303e1491ca65a48f47315611e3b06c6
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-05-31 23:52:48 +0200

    Infra/TF: add instance spawn on Scaleway

---
+++ infrastructure/terraform/instances.tf
@@ -0,0 +1,21 @@
+resource "scaleway_server" "test-instance" {
+  name  = "scw-pcd-kind-demo"
+  type  = "DEV1-XL"
+  # Ubuntu_Bionic_Beaver
+  image = "f974feac-abae-4365-b988-8ec7d1cec10d"
+
+  dynamic_ip_required = true
+  tags = [
+    "paris-container-day",
+    "demo",
+    "kind",
+  ]
+}
+
+output "Instance IP" {
+  value = "${scaleway_server.test-instance.public_ip}"
+}
+
+output "SSH command" {
+  value = "ssh root@${scaleway_server.test-instance.public_ip}"
+}
