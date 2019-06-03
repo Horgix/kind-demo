63a19c56979b636c7ce17b06d6aef4e003046585
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-06-04 01:52:00 +0200

    Demo/02: use a brand new named cluster

--- demo/demo-02.sh
+++ demo/demo-02.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-source functions.sh
+source ./lib/functions.sh
 
 #cmd "Build test tooling" 'cd ~/go/src/k8s.io/kubernetes/ \
 #	&& make WHAT="test/e2e/e2e.test vendor/github.com/onsi/ginkgo/ginkgo"'
 
+cmd "Pop a new Kubernetes cluster with kind" "kind create cluster --name=pcd-demo"
+
 cmd "Tell kubetest where to find kubectl" 'export KUBECTL_PATH=`which kubectl`'
 export KUBECTL_PATH=`which kubectl`
 
-cmd "Tell kubetest what cluster we want to test" 'export KUBECONFIG="$(kind get kubeconfig-path)"'
-export KUBECONFIG=$(kind get kubeconfig-path --name="kind-kubetest")
+cmd "Tell kubetest what cluster we want to test" 'export KUBECONFIG="$(kind get kubeconfig-path --name=pcd-demo)"'
+export KUBECONFIG=$(kind get kubeconfig-path --name=pcd-demo)
 
 cmd "Run a single conformance test with kubetest" 'cd ~/go/src/k8s.io/kubernetes/ \
-        && kubetest --test --provider=local --deployment=local \
-            --test_args="--ginkgo.focus=\[sig-cli\].Kubectl.client.\[k8s.io\].Kubectl.label"'
+	&& kubetest --test --provider=local --deployment=local \
+	    --test_args="--ginkgo.focus=\[sig-cli\].Kubectl.client.\[k8s.io\].Kubectl.label"'
 
 cmd "Run a single conformance test with kubetest... integrated with kind!" 'cd ~/go/src/k8s.io/kubernetes/ \
-        && kubetest --test --provider=local --deployment=kind \
-            --test_args="--ginkgo.focus=\[sig-cli\].Kubectl.client.\[k8s.io\].Kubectl.label"'
+	&& kubetest --test --provider=local --deployment=kind \
+            --kind-cluster-name=pcd-demo \
+	    --test_args="--ginkgo.focus=\[sig-cli\].Kubectl.client.\[k8s.io\].Kubectl.label"'
